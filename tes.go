e5a81c6413e27966f459497b6b5b5bca3516a623
Ignatius Jhon Hezkiel Chan <kielchanyeol@gmail.com>
2024-03-14 20:51:33 +0700

    feat: add beizer for n control points

--- tes.go
+++ tes.go
@@ -1,33 +1,55 @@
 package main
 
 import (
+	"fmt"
 
 	"github.com/fogleman/gg"
 )
 
-type Point struct{
+type Point struct {
 	x float64
 	y float64
 }
 
-func dnc(p1,p2,p3 Point, iterat int) []Point {
-	if iterat==0{
-		arr:=[]Point{p1,p3}
-		return arr
+func midP(point1, point2 Point) Point {
+	var point Point
+	point.x = 0.5 * (point1.x + point2.x)
+	point.y = 0.5 * (point1.y + point2.y)
+	return point
+}
+
+func sol(points []Point) ([]Point, []Point, []Point) {
+	lengthArr := len(points)
+	arr := make([]Point, len(points))
+	leftarr := []Point{points[0]}
+	rightarr := []Point{points[lengthArr-1]}
+
+	copy(arr, points)
+	for lengthArr>=2{
+		arrTemp := make([]Point,lengthArr-1)
+		for i:=0;i<lengthArr-1;i++{
+			arrTemp[i] = midP(arr[i],arr[i+1])
+		}
+		leftarr = append(leftarr, arrTemp[0])
+		rightarr = append([]Point{arrTemp[lengthArr-2]},rightarr...)
+		copy(arr,arrTemp)
+		lengthArr--
 	}
+	return leftarr,[]Point{arr[0]},rightarr
+}
 
-	var lefts,rights,mids Point
-	lefts.x = 0.5*(p1.x+p2.x)
-	lefts.y = 0.5*(p1.y+p2.y)
-	rights.x = 0.5*(p2.x+p3.x)
-	rights.y = 0.5*(p2.y+p3.y)
-	mids.x = 0.5*(lefts.x+rights.x)
-	mids.y = 0.5*(lefts.y+rights.y)
-
-	leftres := dnc(p1,lefts,mids,iterat-1)
-	rightres := dnc(mids,rights,p3,iterat-1)
-	fullres := append(leftres,rightres...)
-	return fullres
+func dnc(points []Point, iterat int) []Point {
+	if iterat == 0 {
+		return make([]Point, 0)
+	}
+	ls, mid, rs := sol(points)
+	fmt.Println(iterat)
+	fmt.Println(ls)
+	fmt.Println(mid)
+	fmt.Println(rs)
+	leftRes := dnc(ls, iterat-1)
+	rightRes := dnc(rs, iterat-1)
+	return append(leftRes, append(mid, rightRes...)...)
 }
 
 func main() {
@@ -40,16 +62,22 @@ func main() {
 	dc.Clear()
 
 	dc.SetRGB(0, 0, 0) // Black color
-	var p1,p2,p3 Point
+	var p1, p2, p3, p4 Point
 	p1.x = 100
 	p1.y = 500
 	p2.x = 400
 	p2.y = 100
 	p3.x = 700
 	p3.y = 550
-	dc.DrawLine(p1.x,p1.y,p2.x,p2.y)
-	dc.DrawLine(p2.x,p2.y,p3.x,p3.y)
-	res := dnc(p1,p2,p3,10)
+	p4.x = 500
+	p4.y = 100
+	dc.DrawLine(p1.x, p1.y, p2.x, p2.y)
+	dc.DrawLine(p2.x, p2.y, p4.x, p4.y)
+	dc.DrawLine(p4.x, p4.y, p3.x, p3.y)
+	res := []Point{p1}
+	res = append(res, dnc([]Point{p1,p2,p4,p3},8)...)
+	res = append(res, p3)
+	// fmt.Println(res)
 	for i:=0;i<len(res)-1;i++{
 		dc.DrawLine(res[i].x,res[i].y,res[i+1].x,res[i+1].y)
 	}
