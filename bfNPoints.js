254c25167566fc84e65b6bda317b0608251e70f8
Ignatius Jhon Hezkiel Chan <kielchanyeol@gmail.com>
2024-03-18 19:35:51 +0700

    fix: update bruteForce

--- bfNPoints.js
+++ bfNPoints.js
@@ -4,8 +4,8 @@ export function bfRecursive(points, t){
     if (points.length==1){
         return points[0];
     }else{
-        let point1 = bfBeizer(points.slice(0,points.length-1),t);
-        let point2 = bfBeizer(points.slice(1),t);
+        let point1 = bfRecursive(points.slice(0,points.length-1),t);
+        let point2 = bfRecursive(points.slice(1),t);
         return new Point(
             ((1-t)*point1.x)+(t*point2.x),
             ((1-t)*point1.y)+(t*point2.y)
